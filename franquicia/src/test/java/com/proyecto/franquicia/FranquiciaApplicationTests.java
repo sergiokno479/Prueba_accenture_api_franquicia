9a5fdb1692f377336b9ef002d8491e51f5bca1b2
Sergio <sergio@ejemplo.com>
2026-04-09 12:18:22 -0500

    inicio de prueba

---
+++ franquicia/src/test/java/com/proyecto/franquicia/FranquiciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.franquicia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
